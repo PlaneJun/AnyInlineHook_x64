6cb7825df44446b34f6183f557cec692f910b6e4
v_trgtang <v_trgtang@tencent.com>
2023-11-07 17:02:21 +0800

    2023/11/07

---
+++ AnyInlineHook/main.cpp
@@ -0,0 +1,30 @@
+﻿// AnyHook.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include "hook/safehook.h"
+
+void hk_TestFunction(safehook::HookContext* ctx)
+{
+    MessageBoxA(NULL, "hook", "hook", NULL);
+}
+
+void TestFunction()
+{
+
+    MessageBoxA(NULL, "TestFunction", "TestFunction", NULL);
+}
+
+int main()
+{
+    safehook hook;
+
+
+    hook.add_hook((uint64_t)TestFunction, (uint64_t)hk_TestFunction);
+    hook.hook();
+
+    TestFunction();
+
+    std::cout << "Hello World!\n";
+}
+
---
+++ README.md
@@ -0,0 +1,15 @@
+
+
+
+
+一个支持x64下的任意inline hook项目，支持相对地址。
+
+
+
+
+
+
+
+# 参考
+
+https://github.com/kanren3/reflector
